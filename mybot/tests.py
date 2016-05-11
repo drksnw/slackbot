df80db47426c6132fd05a7f498a1251d829f0a2c
Guillaume Petitpierre <coomania@gmail.com>
2016-05-11 15:39:25 +0200

    Travis finally working (I hope)

--- mybot/tests.py
+++ mybot/tests.py
@@ -1,13 +1,17 @@
-def test_method(a):
-	"""
-	Returns the value given
 
-	>>> test_method(8)
-	(8)
-	>>> test_method(10)
-	(10)
-	>>> test_method(13)
-	(11)
+def test_my_method():
+	assert 12 == my_method(3, 4), "3 * 4"
+
+def my_method(aa,bb):
+	"""
+	Returns aa * bb
+
+	>>> my_method(8, 2)
+	16
+	>>> my_method(10, 3)
+	30
+	>>> my_method(13, 12)
+	156
 	
 	"""
-	return a
+	return aa*bb
