21f3e7be23fbbffabeeecc3d221ab4e211781ec2
Guillaume Petitpierre <coomania@gmail.com>
2016-06-01 15:33:51 +0200

    Bot functional

--- mybot/bot.py
+++ mybot/bot.py
@@ -8,7 +8,18 @@ import aiohttp
 import websockets
 
 from config import DEBUG, TOKEN
+from prototype import get_forecast
 
+from slackclient import SlackClient
+
+sc = SlackClient(TOKEN)
+print(sc.api_call("api.test"))
+
+def sendMessage(chan, txt):
+    global sc
+    print("Lol :: "+txt)
+    print("I'm sending message to "+chan)
+    return sc.api_call("chat.postMessage", token=TOKEN, as_user="true", channel=chan, text=txt)
 
 RUNNING = True
 
@@ -16,44 +27,67 @@ RUNNING = True
 async def producer():
     """Produce a ping message every 10 seconds."""
     await asyncio.sleep(10)
-    return json.dumps({"id": 1, "type": "message", "channel":"D145QQ6S3", "user":"U0MBP3K6Y", "text":"Ceci est un message"})
+    return json.dumps({"id": 1, "type": "ping"})
 
 
 async def consumer(message):
     """Consume the message by printing them."""
     print(message)
+    dico = json.loads(message)
+    if 'type' in dico and dico['type'] == 'message' and 'channel' in dico and 'text' in dico and 'bot_id' not in dico:
+        vals = dico['text'].split(' ')
+        if len(vals) == 2:
+            print(dico['channel'])
+            #sc.rtm_send_message(dico['channel'], get_forecast(vals[0], vals[1]))
+            print(sendMessage(dico['channel'], get_forecast(vals[0], vals[1])))
+        elif dico['text'].startswith(":middle_finger:"):
+            #Spreads love.
+            for i in range(5):
+                sendMessage(dico['channel'], ":heart:")
+        elif dico['text'] == "42":
+            sendMessage(dico['channel'], "\"Six by nine. Forty two.\"\n\"That's it. That's all there is.\"\n\"I always thought something was fundamentally wrong with the universe\"")
+        elif ":heart:" in dico['text']:
+            sendMessage(dico['channel'], "You can't love a bot...")
+        elif len(vals) > 2:
+            sendMessage(dico['channel'], "Wow, I can't handle that much information !! :scream: \n If your city have multiple words in its name, please write it in one word :wink: \n Example: *La Neuveville CH* becomes *LaNeuveville CH*")
+        else:
+            sendMessage(dico['channel'], "Please send your city followed by the country ! :wink: \nExample: Bern CH\nHave fun ! :full_moon_with_face: :full_moon_with_face:")
+    else:
+        print("That's not interesting")
 
 
 async def bot(token):
     """Create a bot that joins Slack."""
-    loop = asyncio.get_event_loop()
-    with aiohttp.ClientSession(loop=loop) as client:
-        async with client.post("https://slack.com/api/rtm.start",
-                               data={"token": TOKEN}) as response:
-            assert 200 == response.status, "Error connecting to RTM."
-            rtm = await response.json()
+    if sc.rtm_connect():
+        loop = asyncio.get_event_loop()
+        with aiohttp.ClientSession(loop=loop) as client:
+            async with client.post("https://slack.com/api/rtm.start",
+                                   data={"token": TOKEN}) as response:
+                assert 200 == response.status, "Error connecting to RTM."
+                rtm = await response.json()
 
-    async with websockets.connect(rtm["url"]) as ws:
-        while RUNNING:
-            listener_task = asyncio.ensure_future(ws.recv())
-            producer_task = asyncio.ensure_future(producer())
+        async with websockets.connect(rtm["url"]) as ws:
+            while RUNNING:
+                listener_task = asyncio.ensure_future(ws.recv())
+                producer_task = asyncio.ensure_future(producer())
 
-            done, pending = await asyncio.wait(
-                [listener_task, producer_task],
-                return_when=asyncio.FIRST_COMPLETED
-            )
+                done, pending = await asyncio.wait(
+                    [listener_task, producer_task],
+                    return_when=asyncio.FIRST_COMPLETED
+                )
 
-            for task in pending:
-                task.cancel()
+                for task in pending:
+                    task.cancel()
 
-            if listener_task in done:
-                message = listener_task.result()
-                await consumer(message)
+                if listener_task in done:
+                    message = listener_task.result()
+                    await consumer(message)
 
-            if producer_task in done:
-                message = producer_task.result()
-                await ws.send(message)
-
+                if producer_task in done:
+                    message = producer_task.result()
+                    await ws.send(message)
+    else:
+        pass
 
 def stop():
     """Gracefully stop the bot."""
@@ -66,6 +100,6 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     loop.set_debug(DEBUG)
-    loop.add_signal_handler(signal.SIGINT, stop)
+    #loop.add_signal_handler(signal.SIGINT, stop)
     loop.run_until_complete(bot(TOKEN))
     loop.close()
--- mybot/prototype.py
+++ mybot/prototype.py
@@ -8,8 +8,17 @@ def get_forecast(city_name, country):
     url = "http://api.openweathermap.org/data/2.5/weather?q="+city_name+","+country+"&appid="+OWA_TOKEN
     response = requests.get(url)
     jdata = json.loads(response.text)
+    if jdata["cod"] == "404":
+        return "City not found :cry:"
     temp = jdata["main"]["temp"]
     temp -= 273.15 # Temperature given is in Kelvin...
     wea = jdata["weather"][0]["main"]
+    wea_sm = ":question:"
+    if wea == "Clouds":
+        wea_sm = ":cloud:"
+    elif wea == "Rain":
+        wea_sm = ":rain_cloud:"
+    elif wea == "Clear":
+        wea_sm = ":sun_with_face:"
     wea_desc = jdata["weather"][0]["description"]
-    print("Weather forecast for "+city_name+", "+country+"\n"+wea+" : "+wea_desc+"\nTemp: "+str(temp)+"°C")
+    return "Weather forecast for "+jdata["name"]+", "+jdata["sys"]["country"]+"\n"+wea+" "+wea_sm+" : "+wea_desc+"\nTemp: "+str(temp)+"°C"
